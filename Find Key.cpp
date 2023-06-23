9cfe5bc851eb505167394d9f43108c312913a4e4
Mohammed Sehan <2000030708@kluniversity.in>
2023-06-23 16:04:02 +0530

    Create_Find_Key.cpp

---
+++ Find Key.cpp
@@ -0,0 +1,45 @@
+// You are provided with 3 numbers : input1, input2 and input3.
+// Each of these are 4 digits numbers within >=1000 and <=9999
+// i.e.,
+// 1000<=input1<=9999
+// 1000<=input2<=9999
+// 1000<=input3<=9999
+// You are expected to find the key using below formula:
+// Key = Sum of Largest digits of each number + Sum of Second Largest digits of each number
+// For Example, input1=3521, input2=2452 input3=1352
+//       Key = (5+5+5) + (3+4+3) = 25
+// Assuming three numbers are passed to given function and complete the function to find and return the Key
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    int key=0;
+    for(int i=0;i<n;i++)
+    {
+        int LD=INT_MIN,SLD=INT_MIN;
+        while(a[i]>0)
+        {
+            int d=a[i]%10;
+            if(d>LD)
+            {
+                SLD=LD;
+                LD=d;
+            }
+            else if(d<LD && d>SLD)
+            {
+                SLD=d;
+            }
+            a[i]=a[i]/10;
+        }
+        key=LD+SLD+key;
+    }
+    cout<<key;
+    return 0;
+}
